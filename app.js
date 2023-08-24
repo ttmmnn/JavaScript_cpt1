88fee1afdcc8329f3187cb7765c04aceba079dac
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-08-24 20:51:14 +0900

    add_cpt2

--- app.js
+++ app.js
@@ -1,1 +1,61 @@
-alert('Hello World');
+// alert('Hello World');
+
+// 変数
+// let hello = 'Hello World';
+// alert(hello);
+
+// let int1 = 1;
+// alert(int1);
+
+// let int2 = -10;
+// alert(int2);
+
+// let flort = 3.14;
+// alert(flort);
+
+// let str1 = 'JavaScriptを学ぼう';
+// alert(str1);
+
+// 四則演算子
+// alert(4 + 3);
+// alert(8 - 5);
+// alert(2 * 6);
+// alert(10 / 2);
+
+// 条件式
+// let orange = 140;
+// let apple = 120;
+
+// if (orange < apple) {
+//   alert('みかんの値段がりんごより安い');
+// } else if (orange == apple) {
+//   alert('みかんとりんごが同じ値段');
+// } else {
+//   alert('みかんの値段がりんごより高い');
+// }
+
+// 繰り返し処理
+
+// while文
+// let max = 100;
+// // let number = 1;
+// let count = 0;
+
+// while (number < max) {
+//   num = number * 2;
+//   count += 1;
+// }
+
+// // alert(
+//   '2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です'
+// );
+
+// for文
+let i;
+let num = 0;
+
+for (i = 0; i < 11; i++) {
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
