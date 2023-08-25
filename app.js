70fe1c9624dd3e6004b94d0a79f033add95fd576
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-08-25 16:02:30 +0900

    add_cpt_3

--- app.js
+++ app.js
@@ -51,11 +51,92 @@
 // );
 
 // for文
-let i;
-let num = 0;
+// let i;
+// let num = 0;
 
-for (i = 0; i < 11; i++) {
-  num = num + i;
+// for (i = 0; i < 11; i++) {
+//   num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
+// 関数
+
+// let alertString;
+// alertString = addstring('webcamp');
+
+// alert(alertString);
+
+// function addstring(strA) {
+//   let addStr = 'Hello ' + strA;
+//   return addStr;
+// }
+
+// let promptStr = prompt('何か好きな文字を入力してください。');
+// alert(promptStr);
+
+//　じゃんけん
+let user_hand = prompt(
+  'じゃんけんの手をグー、チョキ、パーから選んでください。'
+);
+
+let js_hand = getJShand();
+
+let judge = winLose(user_hand, js_hand);
+
+alert(
+  'あなたの選んだ手は' +
+    user_hand +
+    'です。\nJavaScriptの選んだ手は' +
+    js_hand +
+    'です。\n結果は' +
+    judge +
+    'です。'
+);
+
+function getJShand() {
+  let js_hand_num = Math.floor(Math.random() * 3);
+  let hand_name;
+
+  if (js_hand_num == 0) {
+    hand_name = 'グー';
+  } else if (js_hand_num == 1) {
+    hand_name = 'チョキ';
+  } else if (js_hand_num == 2) {
+    hand_name = 'パー';
+  }
+
+  return hand_name;
 }
 
-alert('1から10まで足し算した結果は' + num + 'です');
+function winLose(user, js) {
+  let winLoseStr;
+
+  if (user == 'グー') {
+    if (js == 'グー') {
+      winLoseStr = 'あいこ';
+    } else if (js == 'チョキ') {
+      winLoseStr = '勝ち';
+    } else if ((js = 'パー')) {
+      winLoseStr = '負け';
+    }
+  } else if (user == 'チョキ') {
+    if (js == 'グー') {
+      winLoseStr = '負け';
+    } else if (js == 'チョキ') {
+      winLoseStr = 'あいこ';
+    } else if ((js = 'パー')) {
+      winLoseStr = '勝ち';
+    }
+  } else if (user == 'パー') {
+    if (js == 'グー') {
+      winLoseStr = '勝ち';
+    } else if (js == 'チョキ') {
+      winLoseStr = '負け';
+    } else if ((js = 'パー')) {
+      winLoseStr = 'あいこ';
+    }
+  }
+
+  return winLoseStr;
+}
