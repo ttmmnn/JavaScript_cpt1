6f5ed344756b6e17dbf7be9cd140b7438bdf5bc8
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-08-25 17:27:23 +0900

    add

--- app.js
+++ app.js
@@ -79,20 +79,33 @@
 let user_hand = prompt(
   'じゃんけんの手をグー、チョキ、パーから選んでください。'
 );
+while (
+  user_hand != 'グー' &&
+  user_hand != 'チョキ' &&
+  user_hand != 'パー' &&
+  user_hand != null
+) {
+  alert('グー・チョキ・パーのいずれかを入力して下さい');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+}
 
 let js_hand = getJShand();
 
 let judge = winLose(user_hand, js_hand);
 
-alert(
-  'あなたの選んだ手は' +
-    user_hand +
-    'です。\nJavaScriptの選んだ手は' +
-    js_hand +
-    'です。\n結果は' +
-    judge +
-    'です。'
-);
+if (user_hand != null) {
+  alert(
+    'あなたの選んだ手は' +
+      user_hand +
+      'です。\nJavaScriptの選んだ手は' +
+      js_hand +
+      'です。\n結果は' +
+      judge +
+      'です。'
+  );
+} else {
+  alert('またチャレンジしてね');
+}
 
 function getJShand() {
   let js_hand_num = Math.floor(Math.random() * 3);
